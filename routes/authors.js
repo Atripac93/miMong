fbc7563e12c96df53ad68a64e0bc5d24903f66f2
Carlo Atripaldi <carloatrica@gmail.com>
2024-03-19 20:56:54 +0100

    Create authors.js

---
+++ routes/authors.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const AuthorsModel = require("../models/authors");
+
+router.get("/getAuthors", async (req, res) => {
+  try {
+    const authors = await AuthorsModel.find();
+    res.status(200).send(authors);
+  } catch (e) {
+    res.status(500).send({
+      statusCode: 500,
+      message: "internal serve error",
+    });
+  }
+});
+
+router.post("/createAuthors", async (req, res) => {
+  const newAuthors = new AuthorsModel({
+    nome: req.body.nome,
+    cognome: req.body.cognome,
+    email: req.body.email,
+    dataDiNascita: req.body.dataDiNascita,
+    avatar: req.body.avatar,
+  });
+  try {
+    const authorsToSave = await newAuthors.save();
+    res.status(201).send({
+      statusCode: 201,
+      payload: authorsToSave,
+    });
+  } catch (e) {
+    res.status(500).send({
+      statusCode: 500,
+      message: "internal problem",
+    });
+  }
+});
+
+module.exports = router;
